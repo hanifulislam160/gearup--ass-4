1318bbe5c11edbe3cf3ac97bb976c6f507f865db
Haniful Islam <hanifulislam160@gmail.com>
2026-07-07 02:38:44 +0600

    express setup and server runiing

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import "dotenv/config";
+// import { prisma } from "./lib/prisma";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+
+const port = config.port;
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Error starting the server:", error);
+    process.exit(1);
+  }
+}
+main();
